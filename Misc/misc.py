1e0375c04dd16b84da95ca404ab38c83e4f2050a
dnackat <dileepbn@gmail.com>
2019-06-20 12:32:00 +0530

    made improvements to decision boundary code..

--- Misc/misc.py
+++ Misc/misc.py
@@ -13,12 +13,15 @@ import matplotlib.pyplot as plt
 x = np.array([[-1,1],[1,-1],[1,1],[2,2]])   # data points
 y = np.array([[1],[1],[-1],[-1]])   # labels
 
-t = np.linspace(-2,2,100)
+# Circular boundary
+t = np.linspace(-5,5,100)
 r = 2.5     # radius
 o1 = -1     # origin x
 o2 = -1     # origin y
 x1 = o1 + r*np.cos(t)   # Circle x
 y1 = o2 + r*np.sin(t)   # Circle y
+
+# Linear boundary
 x_line = np.linspace(-2,2,100)
 theta1 = -1    # offset
 theta2 = -1   # parameter 1
@@ -29,6 +32,6 @@ y_line = (-theta0 - theta1*x_line)/theta2
 plt.figure()
 plt.plot(x[:2,0], x[:2,1], color='red', marker='+', markersize=10, ls = '')
 plt.plot(x[2:,0], x[2:,1], color='blue', marker='o', markersize=8, ls = '')
-#plt.plot(x1,y1,'g-',linewidth=2,label='decision boundary')
+plt.plot(x1,y1,'y-',linewidth=2,label='decision boundary')
 plt.plot(y_line,x_line,'g-',linewidth=2,label='decision boundary')
-plt.legend()
+plt.legend(loc='lower left')
