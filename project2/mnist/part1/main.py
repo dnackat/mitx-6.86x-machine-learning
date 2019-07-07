25c33bfcaec9114ae6c13d0fb4c055c9d265862a
dnackat <dileepbn@gmail.com>
2019-07-08 01:45:01 +0530

    tuned hyperparameters: temp. param., etc..

--- project2/mnist/part1/main.py
+++ project2/mnist/part1/main.py
@@ -114,7 +114,7 @@ def run_softmax_on_MNIST(temp_parameter=1):
     #      and print the test_error_mod3
     return test_error
 
-print('softmax test_error=', run_softmax_on_MNIST(temp_parameter=1))
+print('softmax test_error=', run_softmax_on_MNIST(temp_parameter=1.0))
 
 # TODO: Find the error rate for temp_parameter = [.5, 1.0, 2.0]
 #      Remember to return the tempParameter to 1, and re-run run_softmax_on_MNIST
@@ -130,14 +130,15 @@ def run_softmax_on_MNIST_mod3(temp_parameter=1):
 
     See run_softmax_on_MNIST for more info.
     """
-    #YOUR CODE HERE
-    raise NotImplementedError
+#    train_x, train_y, test_x, test_y = get_MNIST_data()
+#    train_y_mod3, test_y_mod3 = update_y(train_y, test_y)
+#    theta, cost_function_history = softmax_regression(train_x, train_y_mod3, temp_parameter, alpha= 0.3, lambda_factor = 1.0e-4, k = 10, num_iterations = 150)
+#    plot_cost_function_over_time(cost_function_history)
+#    test_error = compute_test_error(test_x, test_y_mod3, theta, temp_parameter)
+#    return test_error
+#
+#print('softmax test_error=', run_softmax_on_MNIST(temp_parameter=1.0))
 #pragma: coderesponse end
-
-
-# TODO: Run run_softmax_on_MNIST_mod3(), report the error rate
-
-
 
 #######################################################################
 # 7. Classification Using Manually Crafted Features
