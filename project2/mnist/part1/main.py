67549e72ff544772a479fa59dec5b3db8ab984bc
dnackat <dileepbn@gmail.com>
2019-07-08 12:39:56 +0530

    compute a 18-dimensional PCA representation of the MNIST training and test datasets...

--- project2/mnist/part1/main.py
+++ project2/mnist/part1/main.py
@@ -40,7 +40,7 @@ def run_linear_regression_on_MNIST(lambda_factor=1):
 
 
 # Don't run this until the relevant functions in linear_regression.py have been fully implemented.
-print('Linear Regression test_error =', run_linear_regression_on_MNIST(lambda_factor=0.01))
+#print('Linear Regression test_error =', run_linear_regression_on_MNIST(lambda_factor=0.01))
 
 
 #######################################################################
@@ -64,7 +64,7 @@ def run_svm_one_vs_rest_on_MNIST():
     return test_error
 
 
-print('SVM one vs. rest test_error:', run_svm_one_vs_rest_on_MNIST())
+#print('SVM one vs. rest test_error:', run_svm_one_vs_rest_on_MNIST())
 
 
 def run_multiclass_svm_on_MNIST():
@@ -80,7 +80,7 @@ def run_multiclass_svm_on_MNIST():
     return test_error
 
 
-print('Multiclass SVM test_error:', run_multiclass_svm_on_MNIST())
+#print('Multiclass SVM test_error:', run_multiclass_svm_on_MNIST())
 
 #######################################################################
 # 4. Multinomial (Softmax) Regression and Gradient Descent
@@ -109,12 +109,16 @@ def run_softmax_on_MNIST(temp_parameter=1):
     test_error = compute_test_error(test_x, test_y, theta, temp_parameter)
     # Save the model parameters theta obtained from calling softmax_regression to disk.
     write_pickle_data(theta, "./theta.pkl.gz")
-
-    # TODO: add your code here for the "Using the Current Model" question in tab 4.
-    #      and print the test_error_mod3
+    
+    # Update labels to label mod 3
+    train_y_mod3, test_y_mod3 = update_y(train_y, test_y)
+    
+    # Compute test error with updated labels
+    test_error = compute_test_error_mod3(test_x, test_y_mod3, theta, temp_parameter)
+    
     return test_error
 
-print('softmax test_error=', run_softmax_on_MNIST(temp_parameter=1.0))
+#print('softmax test_error=', run_softmax_on_MNIST(temp_parameter=1.0))
 
 # TODO: Find the error rate for temp_parameter = [.5, 1.0, 2.0]
 #      Remember to return the tempParameter to 1, and re-run run_softmax_on_MNIST
@@ -130,14 +134,14 @@ def run_softmax_on_MNIST_mod3(temp_parameter=1):
 
     See run_softmax_on_MNIST for more info.
     """
-#    train_x, train_y, test_x, test_y = get_MNIST_data()
-#    train_y_mod3, test_y_mod3 = update_y(train_y, test_y)
-#    theta, cost_function_history = softmax_regression(train_x, train_y_mod3, temp_parameter, alpha= 0.3, lambda_factor = 1.0e-4, k = 10, num_iterations = 150)
-#    plot_cost_function_over_time(cost_function_history)
-#    test_error = compute_test_error(test_x, test_y_mod3, theta, temp_parameter)
-#    return test_error
-#
-#print('softmax test_error=', run_softmax_on_MNIST(temp_parameter=1.0))
+    train_x, train_y, test_x, test_y = get_MNIST_data()
+    train_y_mod3, test_y_mod3 = update_y(train_y, test_y)
+    theta, cost_function_history = softmax_regression(train_x, train_y_mod3, temp_parameter, alpha= 0.3, lambda_factor = 1.0e-4, k = 10, num_iterations = 150)
+    plot_cost_function_over_time(cost_function_history)
+    test_error = compute_test_error(test_x, test_y_mod3, theta, temp_parameter)
+    return test_error
+
+#print('softmax test_error=', run_softmax_on_MNIST_mod3(temp_parameter=1.0))
 #pragma: coderesponse end
 
 #######################################################################
