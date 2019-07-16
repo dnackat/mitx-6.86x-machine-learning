1eb489361d9266a32d262162d434c4e618a97c62
dnackat <dileepbn@gmail.com>
2019-07-16 12:23:48 +0530

    implemented a toy feed forward neural network..

---
+++ Misc/ffNN.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 02:00:08 2019
+
+@author: dileepn
+
+Feedforward Neural Network: Toy Example
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+num_pts = 4
+
+def calc_activation(x,w,bias):
+    z = x.dot(w) + bias.T
+    #f = 5*z - 2
+    f = np.tanh(z)
+    return f
+
+x = np.array([[-1,-1],[1,-1],[-1,1],[1,1]])
+y = np.array([[1],[-1],[-1],[1]])
+
+bias = np.array([[1],[1]])
+w = np.array([[1,-1],[-1,1]]) 
+
+f = calc_activation(x,w,bias)
+print(f)
+
+# Plot data
+colors = ['b' if y == 1 else 'r' for y in y]
+plt.figure()
+plt.scatter(f[:,0], f[:,1], s=40, c=colors)
