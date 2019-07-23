1d05f40c721b83c530ac72d5763f5cc0be69a7ff
dnackat <dileepbn@gmail.com>
2019-07-23 22:46:24 +0530

    coded a neural network from scratch: backprop and prediction

--- project3/mnist/part2-nn/neural_nets.py
+++ project3/mnist/part2-nn/neural_nets.py
@@ -63,28 +63,32 @@ class NeuralNetwork():
         input_values = np.matrix([[x1],[x2]]) # 2 by 1
 
         # Calculate the input and activation of the hidden layer
-        hidden_layer_weighted_input = input_to_hidden_weights.dot(input_values) #(3 by 1 matrix)
+        hidden_layer_weighted_input = self.input_to_hidden_weights.dot(input_values) + self.biases #(3 by 1 matrix)
         ReLU_vec = np.vectorize(rectified_linear_unit)  # Vectorize ReLU function
         hidden_layer_activation = ReLU_vec(hidden_layer_weighted_input) #(3 by 1 matrix)
 
-        output = hidden_to_output_weights.dot(hidden_layer_activation)
-        output_activation_vec = np.vectorize(output_layer_activation)   # Vectorize output activation
-        activated_output = output_activation_vec(output)
+        output = self.hidden_to_output_weights.dot(hidden_layer_activation)
+        #output_activation_vec = np.vectorize(output_layer_activation)   # Vectorize output activation
+        activated_output = output_layer_activation(output)
 
         ### Backpropagation ###
 
         # Compute gradients
-        output_layer_error = # TODO
-        hidden_layer_error = # TODO (3 by 1 matrix)
+        output_layer_error = -(y - activated_output) #dC/df(u1)
+        
+        output_derivative_vec = np.vectorize(output_layer_activation_derivative)    # Vectorize derivative of output activation
+        hidden_layer_error = np.multiply(output_derivative_vec(activated_output),self.hidden_to_output_weights.transpose())*output_layer_error #(3 by 1 matrix)
+        
+        ReLU_derivative_vec = np.vectorize(rectified_linear_unit_derivative) # Vectorize ReLU derivative
+        bias_gradients = np.multiply(hidden_layer_error, ReLU_derivative_vec(hidden_layer_weighted_input)) #dC/db
 
-        bias_gradients = # TODO
-        hidden_to_output_weight_gradients = # TODO
-        input_to_hidden_weight_gradients = # TODO
+        hidden_to_output_weight_gradients = np.multiply(hidden_layer_activation, output_layer_error).transpose() #dC/dV
+        input_to_hidden_weight_gradients = bias_gradients.dot(input_values.transpose()) #dC/dW
 
         # Use gradients to adjust weights and biases using gradient descent
-        self.biases = # TODO
-        self.input_to_hidden_weights = # TODO
-        self.hidden_to_output_weights = # TODO
+        self.biases = self.biases - self.learning_rate*bias_gradients
+        self.input_to_hidden_weights = self.input_to_hidden_weights - self.learning_rate*input_to_hidden_weight_gradients
+        self.hidden_to_output_weights = self.hidden_to_output_weights - self.learning_rate*hidden_to_output_weight_gradients
 #pragma: coderesponse end
 
 #pragma: coderesponse template prefix="class NeuralNetwork(NeuralNetworkBase):\n\n"
@@ -93,10 +97,13 @@ class NeuralNetwork():
         input_values = np.matrix([[x1],[x2]])
 
         # Compute output for a single input(should be same as the forward propagation in training)
-        hidden_layer_weighted_input = # TODO
-        hidden_layer_activation = # TODO
-        output = # TODO
-        activated_output = # TODO
+        hidden_layer_weighted_input = self.input_to_hidden_weights.dot(input_values) + self.biases
+        ReLU_vec = np.vectorize(rectified_linear_unit)
+        hidden_layer_activation = ReLU_vec(hidden_layer_weighted_input)
+        output = self.hidden_to_output_weights.dot(hidden_layer_activation)
+        #output_vec = np.vectorize(output_layer_activation)
+        activated_output = output_layer_activation(output)[0,0]
+        
 
         return activated_output.item()
 #pragma: coderesponse end
@@ -124,4 +131,4 @@ x = NeuralNetwork()
 x.train_neural_network()
 
 # UNCOMMENT THE LINE BELOW TO TEST YOUR NEURAL NETWORK
-# x.test_neural_network()
+x.test_neural_network()
