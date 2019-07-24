28e8d20612b0f6a82f2786b8bc68c058da6caad7
dnackat <dileepbn@gmail.com>
2019-07-24 23:41:56 +0530

    MNIST digit recognition with CNNs using PyTorch..

--- project3/mnist/part2-mnist/nnet_cnn.py
+++ project3/mnist/part2-mnist/nnet_cnn.py
@@ -47,6 +47,13 @@ def main():
               nn.Conv2d(1, 32, (3, 3)),
               nn.ReLU(),
               nn.MaxPool2d((2, 2)),
+              nn.Conv2d(32, 64, (3,3)),
+              nn.ReLU(),
+              nn.MaxPool2d((2, 2)),
+              Flatten(),
+              nn.Linear(1600,128),
+              nn.Dropout(p = 0.5),
+              nn.Linear(128,10)
             )
 #pragma: coderesponse end
     ##################################
