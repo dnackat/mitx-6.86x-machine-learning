178456b0d41d7518e07fd40b700eee27f50389a0
dnackat <dileepbn@gmail.com>
2019-07-24 20:40:01 +0530

    MNIST digit recognition with sequential NNs using PyTorch..

--- project3/mnist/part2-mnist/nnet_fc.py
+++ project3/mnist/part2-mnist/nnet_fc.py
@@ -39,9 +39,9 @@ def main():
     #################################
     ## Model specification TODO
     model = nn.Sequential(
-              nn.Linear(784, 10),
-              nn.ReLU(),
-              nn.Linear(10, 10),
+              nn.Linear(784, 128),
+              nn.LeakyReLU(),
+              nn.Linear(128, 10),
             )
     lr=0.1
     momentum=0
