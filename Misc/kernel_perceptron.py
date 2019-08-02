1297ce86aa36eeba5952c1c6a574451465c5e829
dnackat <dileepbn@gmail.com>
2019-08-02 14:30:25 +0530

    coding kernel perceptron..

--- Misc/kernel_perceptron.py
+++ Misc/kernel_perceptron.py
@@ -18,8 +18,8 @@ y = np.array([[-1],[-1],[-1],[-1],[-1],[1],[1],[1],[1],[1]])
 
 # Plot data
 colors = ['r' if y == 1 else 'b' for y in y]
-plt.figure()
-plt.scatter(x[:,0], x[:,1], s=40, c=colors)
+f, ax = plt.subplots(figsize=(7, 7))
+ax.scatter(x[:,0], x[:,1], s=40, c=colors)
 
 # Number of examples
 n = x.shape[0]
@@ -31,7 +31,9 @@ m = x.shape[1]
 T = 10
 
 # Initialize alphas to zero
-alphas = np.array([[1],[31],[11],[65],[72],[21],[30],[4],[0],[15]])
+alphas = np.zeros((n,1))
+#alphas = np.array([[1],[31],[11],[65],[72],[21],[30],[4],[0],[15]])
+theta0 = 0.0
 
 # Tolerance for floating point errors
 eps = 1e-8
@@ -50,37 +52,47 @@ def quad_kernel(x):
     """
     Returns the kernel representation (quadratic, in this case) of input array, x
     """
-    k1 = x[:,0]**2
-    k2 = x[:,1]**2
-    k12 = np.sqrt(2)*x[:,0]*x[:,1]
-    
-    return np.vstack((k1,k12,k2))
+    try:
+        x.shape[1]
+        k1 = x[:,0]**2
+        k2 = x[:,1]**2
+        k12 = np.sqrt(2)*x[:,0]*x[:,1]
+        return np.vstack((k1,k12,k2))
+    except IndexError:
+        k1 = x[0]**2
+        k2 = x[1]**2
+        k12 = np.sqrt(2)*x[0]*x[1]
+        return np.array([[k1],[k12],[k2]])
 
 # Kernel matrix for our case
-#K = kernel_matrix(x,x)
-#
-## Start kernel perceptron loop
-#for t in range(T):
-#    counter = 0     # To check if all examples are classified correctly in loop
-#    for i in range(n):
-#        agreement = float(y[i]*np.sum(alphas.dot(y.T).dot(K[:,i]), axis=0))
-#        if abs(agreement) < eps or agreement < 0.0:
-#            alphas[i] = alphas[i] + 1
-#        else:
-#            counter += 1
-#        
-#    # If all examples classified correctly, stop
-#    if counter == n:
-#        print("No. of iteration loops through the dataset:", t+1)
-#        break
+indices = np.random.permutation(len(x))
+np.random.shuffle(indices)
+x = x[indices,:]
+y = y[indices]
+K = kernel_matrix(x,x)
+
+# Start kernel perceptron loop
+for t in range(T):
+    counter = 0     # To check if all examples are classified correctly in loop
+    for i in range(n):
+        agreement = float(y[i]*np.sum(alphas.dot(y.T).dot(K[:,i]), axis=0))
+        if abs(agreement) < eps or agreement < 0.0:
+            alphas[i] = alphas[i] + 1
+            theta0 = theta0 + y[i]
+        else:
+            counter += 1
+        
+    # If all examples classified correctly, stop
+    if counter == n:
+        print("No. of iteration loops through the dataset:", t+1)
+        break
 
 theta = np.zeros((3,1))
-theta0 = 0.0
 
 for i in range(n):
     theta = theta + alphas[i]*y[i]*quad_kernel(x[i,:])
-    theta0 = theta0 + float(alphas[i]*y[i])
-    
+    #theta0 = theta0 + float(alphas[i]*y[i])
+
 print("theta0=", theta0)
 print("theta=", theta)
 
@@ -90,8 +102,6 @@ for i in range(n):
     else:
         print("FAIL")
 
-#def decision_boundary(x, theta, theta0):
-#    return         
-## Plot the decision boundary
-#xx, yy = np.meshgrid(np.linspace(-1,6,100), np.linspace(-1,6,100))
-#plt.contour()
+def decision_boundary(x, theta, theta0):
+    return theta.T.dot(quad_kernel(x)) + theta0
+
