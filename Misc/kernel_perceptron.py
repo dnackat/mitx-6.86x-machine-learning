b3ea7fe038d1b409627dab7a34028547a5ef2056
dnackat <dileepbn@gmail.com>
2019-08-03 00:49:19 +0530

    coded kernel perceptron: working as expected

--- Misc/kernel_perceptron.py
+++ Misc/kernel_perceptron.py
@@ -11,10 +11,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Data points
-x = np.array([[0,0],[0,2],[1,1],[2,0],[3,3],[1,4],[4,1],[4,4],[5,2],[5,5]])
+x = np.array([[5,2], [1,4], [2,0], [4,1], [3,3], [4,4], [1,1], [0,2], [5,5], [0,0]])
+#x = np.array([[0,0], [0,2], [1,1], [2,0], [3,3], [1,4], [4,1], [4,4], [5,2], [5,5]])
 
 # Labels
-y = np.array([[-1],[-1],[-1],[-1],[-1],[1],[1],[1],[1],[1]])
+y = np.array([[1],[1],[-1],[1],[-1],[1],[-1],[-1],[1],[-1]])
+#y = np.array([[-1],[-1],[-1],[-1],[-1],[1],[1],[1],[1],[1]])
 
 # Plot data
 colors = ['r' if y == 1 else 'b' for y in y]
@@ -53,7 +55,6 @@ def quad_kernel(x):
     Returns the kernel representation (quadratic, in this case) of input array, x
     """
     try:
-        x.shape[1]
         k1 = x[:,0]**2
         k2 = x[:,1]**2
         k12 = np.sqrt(2)*x[:,0]*x[:,1]
@@ -65,20 +66,23 @@ def quad_kernel(x):
         return np.array([[k1],[k12],[k2]])
 
 # Kernel matrix for our case
-indices = np.random.permutation(len(x))
-x = x[indices,:]
-y = y[indices]
+#indices = np.random.permutation(len(x))
+#x = x[indices,:]
+#y = y[indices]
 K = kernel_matrix(x,x)
 
 # Start kernel perceptron loop
-mistakes = np.zeros((n,1))
 for t in range(T):
     counter = 0     # To check if all examples are classified correctly in loop
     for i in range(n):
-        agreement = float(y[i]*(np.sum(alphas.dot(y.T).dot(K[:,i]), axis=0) + theta0))
+        agreement = 0.0
+        for j in range(n):
+            agreement = agreement + alphas[j]*y[j]*K[j,i]
+        agreement = y[i]*(agreement + theta0)
+        #agreement = float(y[i]*(np.sum(alphas.dot(y.T).dot(K[:,i]), axis=0) + theta0))
+        #agreement = float(y[i]*(y.T.dot(K[:,i]) + theta0))
         if abs(agreement) < eps or agreement < 0.0:
             alphas[i] = alphas[i] + 1
-            mistakes[i] += 1
             theta0 = theta0 + y[i]
         else:
             counter += 1
@@ -86,6 +90,7 @@ for t in range(T):
     # If all examples classified correctly, stop
     if counter == n:
         print("No. of iteration loops through the dataset:", t+1)
+        print("--------------------------------------------------------------")
         break
 
 # Initialize theta vector
@@ -94,7 +99,8 @@ theta = np.zeros((3,1))
 # Calculate theta from calculated alphas
 for i in range(n):
     theta = theta + alphas[i]*y[i]*quad_kernel(x[i,:])
-    #theta0 = theta0 + float(alphas[i]*y[i])
+    #theta = theta + y[i]*quad_kernel(x[i,:])
+    theta0 = theta0 + alphas[i]*y[i]
 
 print("theta0 =", theta0.item())
 print("theta = [{:.2f}, {:.2f}, {:.2f}]".format(theta[0,0], theta[1,0], theta[2,0]))
@@ -102,12 +108,12 @@ print("----------------------------------------------------------------------")
 
 print("=== Mistake Counts ===")
 for i in range(n):
-    print(x[i,:],":\t",mistakes[i].item())
+    print(x[i,:],":\t",alphas[i].item())
 
 print("----------------------------------------------------------------------")
 print("=== Classification Status ===")
+
 for i in range(n):
-    #agreement = float(y[i]*(np.sum(alphas.dot(y.T).dot(K[:,i]), axis=0) + theta0))
     agreement = float(y[i]*(theta.T.dot(quad_kernel(x[i,:])) + theta0))
     if abs(agreement) < eps or agreement < 0.0:
         print("FAIL:\t", x[i,:])
@@ -117,3 +123,9 @@ for i in range(n):
 def decision_boundary(x, theta, theta0):
     return theta.T.dot(quad_kernel(x)) + theta0
 
+xx = np.vstack((np.linspace(-1,6,100), np.linspace(-1,6,100)))
+xx = xx.T
+print(xx.shape)
+a = quad_kernel(xx)
+print(a.shape)
+#ax.contour(decision_boundary(xx, theta, theta0))
