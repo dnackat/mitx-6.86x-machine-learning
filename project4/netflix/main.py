f3dcdb144b10ad7db45771f5358c96908b0dc6d6
dnackat <dileepbn@gmail.com>
2019-08-14 23:01:03 +0530

    ran kMeans algorithm..

--- project4/netflix/main.py
+++ project4/netflix/main.py
@@ -6,4 +6,25 @@ import em
 
 X = np.loadtxt("toy_data.txt")
 
-# TODO: Your code here
+# kMeans vs EM
+K = [1, 2, 3, 4]
+
+seeds = [0, 1, 2, 3, 4]
+
+costs = [0, 0, 0, 0, 0]
+best_seed = [0, 0, 0, 0]
+mixtures = [0, 0, 0, 0, 0]
+posts = [0, 0, 0, 0, 0]
+
+for k in range(len(K)):
+    for i in range(len(seeds)):
+        mixtures[i], posts[i], costs[i] = kmeans.run(X, *common.init(X, K[k], seeds[i]))
+    
+    # Print lowest cost
+    print("Lowest cost for cluster", K[k], "is:", np.min(costs))
+    
+    # Save best seed for plotting
+    best_seed[k] = np.argmin(costs)
+    
+    # Plot for cluster
+    common.plot(X, mixtures[best_seed[k]], posts[best_seed[k]], title="Cluster plot")
