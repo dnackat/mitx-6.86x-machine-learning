25e911e459c6658e1bdbcf5eb980bcfe58276661
dnackat <dileepbn@gmail.com>
2019-08-16 22:57:47 +0530

    modified EM for rating matrix completion: E-step complete..

--- project4/netflix/em.py
+++ project4/netflix/em.py
@@ -22,7 +22,35 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
     mu, var, pi = mixture   # Unpack mixture tuple
     K = mu.shape[0]
     
+    # f(u,j) matrix that's used to store the normal matrix and log of posterior probs: (p(j|u))
+    f = np.zeros((n,K), dtype=np.float64)
     
+    # Compute the normal matrix: Single loop implementation
+    for i in range(n):
+        # For each user pick only columns that have ratings
+        Cu_indices = X[i,:] != 0
+        # Dimension of Cu (no. of non-zero entries)
+        dim = np.sum(np.sum(Cu_indices))
+        # log of pre-exponent for this user's gaussian dist.
+        pre_exp = (-dim/2.0)*np.log((2*np.pi*var))
+        # Calculate the exponent term of the gaussian
+        diff = X[i, Cu_indices] - mu[:, Cu_indices]    # This will be (K,|Cu|)
+        norm = np.sum(diff**2, axis=1)  # This will be (K,)
+        
+        # Now onto the final log normal matrix: log(N(...))
+        # We will need log(normal), exp will cancel, so no need to calculate it
+        f[i,:] = pre_exp - norm/(2*var)  # This is the ith users log p(j|i) vector: (K,)
+    
+    f = f + np.log(pi + 1e-16)  # This is the f(u,j) matrix
+    
+    # log of normalizing term in p(j|u)
+    logsums = logsumexp(f, axis=1).reshape(-1,1)  # Store this to calculate log_lh
+    log_posts = f -  logsums # This is the log of posterior prob. matrix
+    
+    log_lh = np.sum(logsums, axis=0).item()   # This is the log likelihood
+    
+    return np.exp(log_posts), log_lh
+
 
 def mstep(X: np.ndarray, post: np.ndarray, mixture: GaussianMixture,
           min_variance: float = .25) -> GaussianMixture:
