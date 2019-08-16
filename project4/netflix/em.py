c937d2416334c9530afa929224698d8515a08d8c
dnackat <dileepbn@gmail.com>
2019-08-17 00:14:31 +0530

    modified EM for rating matrix completion: working on M-step..

--- project4/netflix/em.py
+++ project4/netflix/em.py
@@ -30,7 +30,7 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
         # For each user pick only columns that have ratings
         Cu_indices = X[i,:] != 0
         # Dimension of Cu (no. of non-zero entries)
-        dim = np.sum(np.sum(Cu_indices))
+        dim = np.sum(Cu_indices)
         # log of pre-exponent for this user's gaussian dist.
         pre_exp = (-dim/2.0)*np.log((2*np.pi*var))
         # Calculate the exponent term of the gaussian
@@ -39,13 +39,13 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
         
         # Now onto the final log normal matrix: log(N(...))
         # We will need log(normal), exp will cancel, so no need to calculate it
-        f[i,:] = pre_exp - norm/(2*var)  # This is the ith users log p(j|i) vector: (K,)
+        f[i,:] = pre_exp - norm/(2*var)  # This is the ith users log gaussian dist vector: (K,)
     
     f = f + np.log(pi + 1e-16)  # This is the f(u,j) matrix
     
     # log of normalizing term in p(j|u)
     logsums = logsumexp(f, axis=1).reshape(-1,1)  # Store this to calculate log_lh
-    log_posts = f -  logsums # This is the log of posterior prob. matrix
+    log_posts = f -  logsums # This is the log of posterior prob. matrix: log(p(j|u))
     
     log_lh = np.sum(logsums, axis=0).item()   # This is the log likelihood
     
@@ -67,7 +67,16 @@ def mstep(X: np.ndarray, post: np.ndarray, mixture: GaussianMixture,
     Returns:
         GaussianMixture: the new gaussian mixture
     """
-    raise NotImplementedError
+    n, d = X.shape
+    
+    # Calculate revised pi(j): same expression as in the naive case
+    pi = np.sum(post, axis=0)/n
+    
+    # Create delta matrix indicating where X is non-zero
+    delta = X.astype(bool).astype(int)
+    
+    # Update means only when sum_u(p(j|u)*delta(l,Cu)) >= 1
+    
 
 
 def run(X: np.ndarray, mixture: GaussianMixture,
