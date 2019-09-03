4726be261ae86643dad3ac88e24417f11c391372
dnackat <dileepbn@gmail.com>
2019-09-03 19:09:18 +0530

    Coding challenge: multiple linear reg. to predict house prices..

---
+++ Misc/regression_HousePricing.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  3 18:44:03 2019
+
+@author: dileepn
+"""
+import numpy as np
+import sklearn
+import matplotlib.pyplot as plt
+
+#%% Open the dataset and define X, y, and m
+def load_data():
+    """ This function loads: 
+        1. Space-separated text (*.txt) file using numpy
+        and returns X and y as numpy arrays. Sample input (last column is y):
+            2 100
+            0.44 0.68 511.14
+            0.99 0.23 717.1
+            0.84 0.29 607.91
+            0.28 0.45 270.4
+            0.07 0.83 289.88
+                    .
+                    .
+                    .
+            4
+            0.05 0.54
+            0.91 0.91
+            0.31 0.76
+            0.51 0.31
+            
+                    OR
+        
+        2. Data entered manually (space-separated) on the standard input 
+        and stores them in X and y. """
+
+    while True:
+        
+        # Prompt user for dataset input type
+        input_type = input("Choose dataset input type. 1 (file) or 2 (manual entry): ")
+    
+        if input_type == "1":
+            
+            # Prompt for filepath
+            filepath = input("Enter the complete filepath (/home/user...): ")
+            
+            # Temporary lists to store data as it is being read
+            temp_data = []
+            temp_test_data = []
+
+            # Read the dataset line-by-line. Get num. of features, F, and 
+            # num. of examples, N
+            with open(filepath) as input_file:
+            
+                for line_num, line in enumerate(input_file):
+                    if line_num == 0:
+                        F, N = line.split()
+                        F, N = int(F), int(N)
+                    elif line_num == N + 1:
+                        T = int(line)
+                    elif line_num > 0 and line_num <= N:
+                        x1, x2, y = line.split()
+                        # Store as ordered pair in temp_data
+                        temp_data += [(float(x1), float(x2), float(y))]
+                    elif line_num > N + 1 and line_num <= N + T + 1:
+                        x1, x2 = line.split()
+                        temp_test_data += [(float(x1), float(x2))]
+                    
+            # Convert temp lists into numpy arrays
+            dataset = np.array(temp_data)
+            X_pred = np.array(temp_test_data)       
+            
+            # Define X, y, and m
+            X = dataset[:, :F]
+            y = dataset[:, F]
+            # Convert y to rank 2 array
+            y = y[:, np.newaxis]
+            
+            break
+            
+        elif input_type == "2":
+            
+            # First line has number of features and number of training examples
+            F, N = map(int, input().split())
+            
+            # Get the training set (X and y)
+            train = np.array([input().split() for _ in range(N)], float)
+            
+            # Number of test examples
+            T = int(input())
+            X_pred = np.array([input().split() for _ in range(T)], float)
+            
+            # Split the training set into X and y
+            X = train[:,:F]
+            y = train[:,F]
+            
+            break
+        
+        else:
+            print("Incorrect input. Please enter 1 or 2.")
+    
+    return (X, y, X_pred)
+
+# Load data
+X, y, X_pred = load_data()
+
+# Split the data into train and cross-validation sets
+X_train, X_cv, y_train, y_cv = sklearn.model_selection.train_test_split(X, y)
+
+# Fit the model and predict; use CV set to tune hyperparams.
